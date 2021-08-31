4e333261cb6178f3f2fc45705ec8a2db25a316d3
Ian Dunn <ian@iandunn.name>
2021-08-31 09:21:43 -0700

    add rough content and a few styles

--- mu-plugins/blocks/global-header-footer/universal-footer.php
+++ mu-plugins/blocks/global-header-footer/universal-footer.php
@@ -1,5 +1,29 @@
 <?php
-echo 'this is the global footer :)';
-return;
 
+namespace WordPressdotorg\MU_Plugins\Global_Header_Footer\Footer;
 
+defined( 'WPINC' ) || die();
+
+?>
+
+<!-- wp:group {"tagName":"footer","backgroundColor":"dark-strokes-grey","className":"site-footer","layout":{"inherit":true}} -->
+<footer class="wp-block-group site-footer has-dark-strokes-grey-background-color has-background"><!-- wp:list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} -->
+<ul class="has-link-color"><li><a href="https://wordpress.org/about/">About</a></li><li><a href="https://wordpress.org/news/">Blog</a></li><li><a href="https://wordpress.org/hosting/">Hosting</a></li><li><a href="https://wordpressfoundation.org/donate/">Donate</a></li><li><a href="https://wordpress.org/support/">Support</a></li><li><a href="https://developer.wordpress.org/">Developers</a></li><li><a href="https://make.wordpress.org/">Get Involved</a></li><li><a href="https://learn.wordpress.org/">Learn</a></li><li><a href="https://wordpress.org/showcase/">Showcase</a></li><li><a href="https://wordpress.org/plugins/">Plugins</a></li><li><a href="https://wordpress.org/themes/">Themes</a></li><li><a href="https://wordpress.org/patterns/">Patterns</a></li><li><a href="https://central.wordcamp.org/">WordCamp</a></li><li><a href="https://wordpress.org/five-for-the-future/#:~:text=WordCamp-,WordPress.TV,-BuddyPress">WordPress.TV</a></li><li><a href="https://buddypress.org/">BuddyPress</a></li><li><a href="https://bbpress.org/">bbPress</a></li><li><a href="https://wordpress.com/?ref=wporg-footer">WordPress.com</a></li><li><a href="https://ma.tt/">Matt</a></li><li><a href="https://wordpress.org/about/privacy/">Privacy</a></li><li><a href="https://publiccode.eu/">Public Code</a></li><li><a href="https://twitter.com/WordPress">@WordPress</a></li><li><a href="https://www.facebook.com/WordPress/">WordPress</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:group {"className":"footer-logos-container","layout":{"inherit":true}} -->
+<div class="wp-block-group footer-logos-container"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://s.w.org/style/images/wporg-logo.svg?3" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://s.w.org/style/images/codeispoetry.png?1" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:social-links -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.facebook.com/WordPress/","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"https://twitter.com/WordPress","service":"twitter","label":""} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></footer>
+<!-- /wp:group -->
--- mu-plugins/blocks/global-header-footer/universal-header.php
+++ mu-plugins/blocks/global-header-footer/universal-header.php
@@ -1,4 +1,36 @@
 <?php
-echo 'this is the global header :)';
-return;
 
+namespace WordPressdotorg\MU_Plugins\Global_Header_Footer\Header;
+
+defined( 'WPINC' ) || die();
+
+$is_fse_theme = true; // temp
+
+// meta tags included called automaticaly in FSE themes
+if ( ! $is_fse_theme ) {
+	wp_head();
+}
+
+?>
+
+<!-- wp:group {"tagName":"header","backgroundColor":"dark-strokes-grey","className":"site-header","layout":{"inherit":true}} -->
+<header class="wp-block-group site-header has-dark-strokes-grey-background-color has-background"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://s.w.org/style/images/wporg-logo.svg?3" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} -->
+<ul class="has-link-color"><li><a href="https://wordpress.org/plugins/">Plugins</a></li><li><a href="https://wordpress.org/themes/">Themes</a></li><li><a href="https://wordpress.org/patterns/">Patterns</a></li><li><a href="https://learn.wordpress.org/">Learn</a></li><li><a href="https://wordpress.org/support/">Support</a></li><li><a href="https://wordpress.org/news/">News</a></li><li><a href="https://wordpress.org/about/">About</a></li><li><a href="https://make.wordpress.org/">Get Involved</a></li><li><a href="https://wordpress.org/showcase/">Showcase</a></li><li><a href="https://wordpress.org/mobile/">Mobile</a></li><li><a href="https://wordpress.org/hosting/">Hosting</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"dark-strokes-grey","style":{"border":{"radius":"0px"}},"className":"site-header__search"} -->
+<div class="wp-block-button site-header__search"><a class="wp-block-button__link has-dark-strokes-grey-background-color has-background" style="border-radius:0px">Search</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"site-header__get_wordpress"} -->
+<div class="wp-block-button site-header__get_wordpress"><a class="wp-block-button__link">Get WordPress</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></header>
+<!-- /wp:group -->
