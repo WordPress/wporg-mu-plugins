0c26f55e3ba0de06f44237446109acff4397628d
Ian Dunn <ian@iandunn.name>
2021-10-25 10:40:37 -0700

    Header/Footer: Rename classes to "global header" for clarity.
    See https://github.com/WordPress/wporg-news-2021/pull/41#discussion_r733949263

--- mu-plugins/blocks/global-header-footer/header.php
+++ mu-plugins/blocks/global-header-footer/header.php
@@ -13,19 +13,19 @@ if ( ! $is_fse_theme ) {
 
 ?>
 
-<!-- wp:group {"tagName":"header","align":"full","className":"site-header"} -->
-<header class="wp-block-group site-header alignfull">
+<!-- wp:group {"tagName":"header","align":"full","className":"global-header"} -->
+<header class="wp-block-group global-header alignfull">
 	<!-- The design calls for two logos, a small "mark" on mobile/tablet, and the full logo for desktops. -->
-	<!-- wp:image {"width":27,"height":27,"className":"site-header__wporg-logo-mark"} -->
-	<figure class="wp-block-image is-resized site-header__wporg-logo-mark">
+	<!-- wp:image {"width":27,"height":27,"className":"global-header__wporg-logo-mark"} -->
+	<figure class="wp-block-image is-resized global-header__wporg-logo-mark">
 		<a href="https://wordpress.org/">
 			<img src="https://wordpress.org/style/images/w-mark.svg" alt="" width="27" height="27" />
 		</a>
 	</figure>
 	<!-- /wp:image -->
 
-	<!-- wp:image {"width":160,"height":24,"className":"site-header__wporg-logo-full"} -->
-	<figure class="wp-block-image is-resized site-header__wporg-logo-full">
+	<!-- wp:image {"width":160,"height":24,"className":"global-header__wporg-logo-full"} -->
+	<figure class="wp-block-image is-resized global-header__wporg-logo-full">
 		<a href="https://wordpress.org/">
 			<img src="https://wordpress.org/style/images/wporg-logo.svg?3" alt="" width="160" height="24" />
 		</a>
@@ -37,7 +37,7 @@ if ( ! $is_fse_theme ) {
 		calls for. It also provides a consistent experience with the primary navigation menu, with respect to
 		keyboard navigation, ARIA states, etc. It also saves having to write custom code for all the interactions.
 	-->
-	<!-- wp:navigation {"orientation":"vertical","className":"site-header__search","isResponsive":true} -->
+	<!-- wp:navigation {"orientation":"vertical","className":"global-header__search","isResponsive":true} -->
 		<!-- wp:navigation-link {"label":"Search","url":"#","kind":"custom","isTopLevelLink":false} -->
 			<!-- wp:html -->
 			<!--
@@ -58,7 +58,7 @@ if ( ! $is_fse_theme ) {
 				role="search"
 				method="get"
 				action="https://wordpress.org/search/do-search.php"
-				class="wp-block-search__button-outside wp-block-search__text-button site-header__search-form wp-block-search"
+				class="wp-block-search__button-outside wp-block-search__text-button global-header__search-form wp-block-search"
 			>
 				<label for="wp-block-search__input-99" class="wp-block-search__label">Search</label>
 				<div class="wp-block-search__inside-wrapper">
@@ -80,9 +80,9 @@ if ( ! $is_fse_theme ) {
 
 	<!-- This is the first of two Get WordPress buttons; the other is in the navigation menu.
 		 Two are needed because they have different DOM hierarchies at different breakpoints. -->
-	<!-- wp:group {"className":"site-header__desktop-get-wordpress-container"} -->
-	<div class="site-header__desktop-get-wordpress-container">
-		<a href="https://wordpress.org/download/" class="site-header__desktop-get-wordpress site-header__get-wordpress">Get WordPress</a>
+	<!-- wp:group {"className":"global-header__desktop-get-wordpress-container"} -->
+	<div class="global-header__desktop-get-wordpress-container">
+		<a href="https://wordpress.org/download/" class="global-header__desktop-get-wordpress global-header__get-wordpress">Get WordPress</a>
 	</div> <!-- /wp:group -->
 
 	<!--
@@ -91,7 +91,7 @@ if ( ! $is_fse_theme ) {
 		items should be moved to the top level (e.g., Five for the Future), to make them easier to see, and to
 		make the CSS simpler.
 	-->
-	<!-- wp:navigation {"orientation":"horizontal","className":"site-header__navigation","isResponsive":true} -->
+	<!-- wp:navigation {"orientation":"horizontal","className":"global-header__navigation","isResponsive":true} -->
 		<!-- wp:navigation-link {"label":"Plugins","url":"https://wordpress.org/plugins/","kind":"custom","isTopLevelLink":true} /-->
 		<!-- wp:navigation-link {"label":"Themes","url":"https://wordpress.org/themes/","kind":"custom","isTopLevelLink":true} /-->
 		<!-- wp:navigation-link {"label":"Patterns","url":"https://wordpress.org/patterns/","kind":"custom","isTopLevelLink":true} /-->
@@ -101,15 +101,15 @@ if ( ! $is_fse_theme ) {
 			<!-- wp:navigation-link {"label":"Forums","url":"https://wordpress.org/support/forums/","kind":"custom","isTopLevelLink":true} /-->
 		<!-- /wp:navigation-link -->
 		<!-- wp:navigation-link {"label":"News","url":"https://wordpress.org/news","kind":"custom","isTopLevelLink":true,"className":"current-menu-item"} /-->
-		<!-- wp:navigation-link {"label":"About","url":"https://wordpress.org/about/","kind":"custom","isTopLevelLink":true,"className":"site-header__overflow-item"} /-->
-		<!-- wp:navigation-link {"label":"Get Involved","url":"https://make.wordpress.org/","kind":"custom","isTopLevelLink":false,"className":"site-header__overflow-item"} -->
+		<!-- wp:navigation-link {"label":"About","url":"https://wordpress.org/about/","kind":"custom","isTopLevelLink":true,"className":"global-header__overflow-item"} /-->
+		<!-- wp:navigation-link {"label":"Get Involved","url":"https://make.wordpress.org/","kind":"custom","isTopLevelLink":false,"className":"global-header__overflow-item"} -->
 			<!-- wp:navigation-link {"label":"Five for the Future","url":"https://wordpress.org/five-for-the-future/","kind":"custom","isTopLevelLink":true} /-->
 		<!-- /wp:navigation-link -->
-		<!-- wp:navigation-link {"label":"Showcase","url":"https://wordpress.org/showcase/","kind":"custom","isTopLevelLink":true,"className":"site-header__overflow-item"} /-->
-		<!-- wp:navigation-link {"label":"Mobile","url":"https://wordpress.org/mobile/","kind":"custom","isTopLevelLink":true,"className":"site-header__overflow-item"} /-->
-		<!-- wp:navigation-link {"label":"Hosting","url":"https://wordpress.org/hosting/","kind":"custom","isTopLevelLink":true,"className":"site-header__overflow-item"} /-->
-		<!-- wp:navigation-link {"label":"Openverse","url":"https://wordpress.org/openverse/","kind":"custom","isTopLevelLink":true,"className":"site-header__overflow-item"} /-->
-		<!-- wp:navigation-link {"label":"...","url":"#","kind":"custom","isTopLevelLink":false,"className":"site-header__overflow-menu"} -->
+		<!-- wp:navigation-link {"label":"Showcase","url":"https://wordpress.org/showcase/","kind":"custom","isTopLevelLink":true,"className":"global-header__overflow-item"} /-->
+		<!-- wp:navigation-link {"label":"Mobile","url":"https://wordpress.org/mobile/","kind":"custom","isTopLevelLink":true,"className":"global-header__overflow-item"} /-->
+		<!-- wp:navigation-link {"label":"Hosting","url":"https://wordpress.org/hosting/","kind":"custom","isTopLevelLink":true,"className":"global-header__overflow-item"} /-->
+		<!-- wp:navigation-link {"label":"Openverse","url":"https://wordpress.org/openverse/","kind":"custom","isTopLevelLink":true,"className":"global-header__overflow-item"} /-->
+		<!-- wp:navigation-link {"label":"...","url":"#","kind":"custom","isTopLevelLink":false,"className":"global-header__overflow-menu"} -->
 			<!-- wp:navigation-link {"label":"About","url":"https://wordpress.org/about/","kind":"custom","isTopLevelLink":true} /-->
 			<!-- wp:navigation-link {"label":"Get Involved","url":"https://make.wordpress.org/","kind":"custom","isTopLevelLink":true} /-->
 			<!-- wp:navigation-link {"label":"Five for the Future","url":"https://wordpress.org/five-for-the-future/","kind":"custom","isTopLevelLink":true} /-->
@@ -118,6 +118,6 @@ if ( ! $is_fse_theme ) {
 			<!-- wp:navigation-link {"label":"Hosting","url":"https://wordpress.org/hosting/","kind":"custom","isTopLevelLink":true} /-->
 			<!-- wp:navigation-link {"label":"Openverse","url":"https://wordpress.org/openverse/","kind":"custom","isTopLevelLink":true} /-->
 		<!-- /wp:navigation-link -->
-		<!-- wp:navigation-link {"label":"Get WordPress","url":"https://wordpress.org/download/","kind":"custom","isTopLevelLink":true,"className":"site-header__mobile-get-wordpress site-header__get-wordpress"} /-->
+		<!-- wp:navigation-link {"label":"Get WordPress","url":"https://wordpress.org/download/","kind":"custom","isTopLevelLink":true,"className":"global-header__mobile-get-wordpress global-header__get-wordpress"} /-->
 	<!-- /wp:navigation -->
 </header> <!-- /wp:group -->
