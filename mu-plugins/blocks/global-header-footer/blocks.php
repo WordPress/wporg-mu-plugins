f5835d27b9c3582550d692ff34014c2bf4286d97
Ian Dunn <ian@iandunn.name>
2021-12-17 13:41:12 -0800

    Header/Footer: Output dependent FSE styles in Classic themes.
    There are many minor bugs to fix, but this is the core of the solution.
    
    See https://github.com/WordPress/wporg-mu-plugins/issues/19

--- mu-plugins/blocks/global-header-footer/blocks.php
+++ mu-plugins/blocks/global-header-footer/blocks.php
@@ -6,6 +6,8 @@ defined( 'WPINC' ) || die();
 
 add_action( 'init', __NAMESPACE__ . '\register_block_types' );
 add_action( 'enqueue_block_assets', __NAMESPACE__ . '\register_block_types_js' );
+add_filter( 'pre_set_transient_global_styles_wporg-news-2021', __NAMESPACE__ . '\save_dependent_global_styles' );
+
 
 /**
  * Register block types
@@ -91,16 +93,51 @@ function register_block_types_js() {
 }
 
 /**
+ * Remove the wrapping element to preserve markup.
+ *
+ * Core and Gutenberg add a wrapper `div` for backwards-compatibility, but that is unnecessary here, and breaks
+ * CSS selectors.
+ *
+ * @see restore_inner_group_container()
+ */
+function remove_inner_group_container() {
+	if ( ! wp_is_block_theme() ) {
+		remove_filter( 'render_block', 'wp_restore_group_inner_container' );
+		remove_filter( 'render_block', 'gutenberg_restore_group_inner_container' );
+	}
+}
+
+/**
+ * Restore the wrapping element to prevent side-effects on the content area.
+ *
+ * @see remove_inner_group_container()
+ */
+function restore_inner_group_container() {
+	if ( ! wp_is_block_theme() ) {
+		if ( function_exists( 'gutenberg_restore_group_inner_container' ) ) {
+			add_filter( 'render_block', 'gutenberg_restore_group_inner_container', 10, 2 );
+		} else {
+			add_filter( 'render_block', 'wp_restore_group_inner_container', 10, 2 );
+		}
+	}
+}
+
+/**
  * Render the global header in a block context.
  *
  * @return string
  */
 function render_global_header() {
-	ob_start();
+	remove_inner_group_container();
 
+	ob_start();
 	// Allow multiple includes for the `site-header-offset` workaround.
 	require __DIR__ . '/header.php';
-	return do_blocks( ob_get_clean() );
+	$markup = do_blocks( ob_get_clean() );
+
+	restore_inner_group_container();
+
+	return $markup;
 }
 
 /**
@@ -109,7 +146,44 @@ function render_global_header() {
  * @return string
  */
 function render_global_footer() {
+	remove_inner_group_container();
+
 	ob_start();
 	require_once __DIR__ . '/footer.php';
-	return do_blocks( ob_get_clean() );
+	$markup = do_blocks( ob_get_clean() );
+
+	restore_inner_group_container();
+
+	return $markup;
+}
+
+/**
+ * Save the FSE global styles that the global header/footer depends on.
+ *
+ * The header/footer blocks are built primarily for block themes, but also need to work in Classic themes. The
+ * styles that the News site generates are saved, so that they can later be loaded for Classic themes.
+ *
+ * @see `wp_get_global_stylesheet()`
+ *
+ * @param string $news_transient_value
+ *
+ * @return string
+ */
+function save_dependent_global_styles( $news_transient_value ) {
+	/*
+	 * There could be multiple sites running the News theme -- like `/news` and `/news-test` -- so make sure that
+	 * only the production styles are used.
+	 */
+	if ( 706 === get_current_blog_id() ) { // `w.org/news-test`. TODO change this to `w.org/news` when the redesign launches.
+		return $news_transient_value;
+	}
+
+	$network_option_value = get_network_option( 1, 'global-header-footer-dependent-styles' );
+
+	if ( $network_option_value !== $news_transient_value ) {
+		update_network_option( 1, 'global-header-footer-dependent-styles', $news_transient_value );
+	}
+
+	// We don't want to change the value, using this filter is just a way to access it when it changes.
+	return $news_transient_value;
 }
--- mu-plugins/blocks/global-header-footer/header.php
+++ mu-plugins/blocks/global-header-footer/header.php
@@ -4,14 +4,36 @@ namespace WordPressdotorg\MU_Plugins\Global_Header_Footer\Header;
 
 defined( 'WPINC' ) || die();
 
-$is_fse_theme = true; // temp
+/*
+ * `template-canvas.php` provides similar markup automatically for FSE templates, but Classic themes need it
+ *  explicitly declared.
+ */
+if ( ! wp_is_block_theme() ) :
+	?><!DOCTYPE html>
+	<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-// Meta tags are included automatically in FSE themes.
-if ( ! $is_fse_theme ) {
-	wp_head();
-}
+		<!--
+		  Output the FSE styles that the header/footer relies on.
 
-?>
+		  Some of them may conflict with rules that the Classic theme applies to the content area. These are
+		  output first, so they can be overridden if needed.
+
+		  This is essentially a cached version of the output of `wp_get_global_stylesheet()` in an FSE theme.
+		  @see `save_dependent_global_styles()`
+		-->
+		<style id="global-styles-for-classic-themes">
+			<?php echo get_network_option( 1, 'global-header-footer-dependent-styles' ); ?>
+		</style>
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+<?php endif; ?>
 
 <!-- wp:group {"tagName":"header","align":"full","className":"global-header"} -->
 <header class="wp-block-group global-header alignfull">
