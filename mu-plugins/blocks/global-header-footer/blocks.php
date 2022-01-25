61a597266f95ca6ac69eb86e9cbb6663f22c338d
Dion Hulse <dion@wordpress.org>
2022-01-25 16:14:47 +1000

    Bump wp4 CSS version.

--- mu-plugins/blocks/global-header-footer/blocks.php
+++ mu-plugins/blocks/global-header-footer/blocks.php
@@ -213,7 +213,7 @@ function enqueue_compat_wp4_styles() {
 			'wp4-styles',
 			'https://s.w.org/style/wp4' . $suffix . '.css',
 			array( 'open-sans' ),
-			96
+			97
 		);
 
 		wp_enqueue_style( 'wp4-styles' );
