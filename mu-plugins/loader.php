28720671fed1239c5be965af3ef45ff043c6ac93
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2022-02-24 15:03:47 -0800

    Add an mu-plugin for tweaking the Stream plugin to meet our needs
    For now this replaces Stream's posts connector with our own modified
    version so that it will log post changes correctly when made in the
    block editor. Other tweaks may follow.

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -6,3 +6,4 @@
 require_once __DIR__ . '/blocks/global-header-footer/blocks.php';
 require_once __DIR__ . '/global-fonts/index.php';
 require_once __DIR__ . '/skip-to/skip-to.php';
+require_once __DIR__ . '/stream-tweaks/index.php';
