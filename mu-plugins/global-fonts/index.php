5b1e3a52e0f95aaa9d14489b8dd42f201e68fd76
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-24 15:34:40 -0500

    Fonts: Add the full URL when loading styles into the editor.

--- mu-plugins/global-fonts/index.php
+++ mu-plugins/global-fonts/index.php
@@ -7,6 +7,7 @@
 namespace WordPressdotorg\MU_Plugins\Global_Fonts;
 
 add_filter( 'init', __NAMESPACE__ . '\register_style', 1 );
+add_filter( 'block_editor_settings_all', __NAMESPACE__ . '\relative_to_absolute_urls' );
 
 /**
  * Register stylesheet with font-family declarations.
@@ -21,4 +22,27 @@ function register_style() {
  */
 function get_font_stylesheet_url() {
 	return plugins_url( 'style.css', __FILE__ );
+}
+
+/**
+ * Filter the styles added by editor settings to inject the full URL to the font files.
+ *
+ * Once inlined in wp-admin, the relative URLs don't match the correct file paths.
+ *
+ * @param array $editor_settings Default editor settings.
+ */
+function relative_to_absolute_urls( $editor_settings ) {
+	if ( ! isset( $editor_settings['styles'] ) ) {
+		return $editor_settings;
+	}
+
+	foreach ( $editor_settings['styles'] as $i => $style ) {
+		if ( str_contains( $style['css'], './Inter' ) || str_contains( $style['css'], './EB-Garamond' ) ) {
+			$url = plugins_url( '', __FILE__ );
+			$style['css'] = str_replace( 'url(./', "url($url/", $style['css'] );
+			$editor_settings['styles'][ $i ] = $style;
+		}
+	}
+
+	return $editor_settings;
 }
