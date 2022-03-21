821950b5af246831f4307cba1d33172de651fd96
dufresnesteven <steve.dufresne@automattic.com>
2022-03-21 14:51:05 +0900

    Revert "Skip adding current to downloads button."
    This reverts commit d33b622cd517fc19a52523f5c240316fb0aee777.

--- mu-plugins/blocks/global-header-footer/blocks.php
+++ mu-plugins/blocks/global-header-footer/blocks.php
@@ -975,12 +975,6 @@ function set_current_item_class( $menu_items ) {
 
 	foreach ( $menu_items as & $item ) {
 		$sub = false;
-
-		// The download menu item is a button, we don't highlight it
-		if ( get_download_url() === $item['url'] ) {
-			continue;
-		}
-
 		if ( ! empty( $item['submenu'] ) ) {
 			foreach ( $item['submenu'] as & $subitem ) {
 				if ( $current_url === $subitem['url'] ) {
