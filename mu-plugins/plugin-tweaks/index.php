75a81fa8e2fbd9230993d09dbba5b8ee4c5cce26
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2022-03-31 18:14:48 -0700

    Add a "plugin tweaks" module, and add a tweak for Internal Notes (#192)
    Creates a new subdirectory within mu-plugins for keeping small tweaks and one-liner changes for other plugins in the wporg network. Starting with a tweak for the Internal Notes plugin.

---
+++ mu-plugins/plugin-tweaks/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Tweaks for specific plugins on WordPress.org.
+ *
+ * Each plugin gets its own file or subdirectory in the `plugin-tweaks` directory.
+ */
+
+namespace WordPressdotorg\MU_Plugins\Plugin_Tweaks;
+
+defined( 'WPINC' ) || die();
+
+require_once __DIR__ . '/wporg-internal-notes.php';
