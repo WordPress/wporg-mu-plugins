51fd6af359bfc6b0374430275a6d67971049f819
Dion Hulse <dion@wordpress.org>
2022-05-12 04:04:44 +0000

    Load the CDN plugin.

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -8,3 +8,4 @@ require_once __DIR__ . '/global-fonts/index.php';
 require_once __DIR__ . '/plugin-tweaks/index.php';
 require_once __DIR__ . '/rest-api/index.php';
 require_once __DIR__ . '/skip-to/skip-to.php';
+require_once __DIR__ . '/cdn/cdn-assets.php.php';
