74820317076ecdb5e918d0fbd122cd1afced5cd7
Dion Hulse <dion@wordpress.org>
2022-05-12 05:11:02 +0000

    Add PHPUnit bootstrapping

---
+++ phpunit/test-cdn-assets.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test_CDN_Assets extends WP_UnitTestCase {
+
+	public function test_tests_work() {
+		$this->assertTrue( true );
+	}
+
+	public function test_tests_error() {
+		$this->assertTrue( false );
+	}
+
+}
