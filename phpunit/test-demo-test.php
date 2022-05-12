8f4d7f6c284b55a3c2bef7f1ed31c1dfbd2c4d26
Dion Hulse <dion@wordpress.org>
2022-05-12 05:11:02 +0000

    Add PHPUnit bootstrapping

---
+++ phpunit/test-demo-test.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test_Demo_Test extends WP_UnitTestCase {
+
+	public function test_tests_work() {
+		$this->assertTrue( true );
+	}
+
+	public function test_tests_error() {
+		$this->assertTrue( false );
+	}
+
+}
