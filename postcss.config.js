e1d556bb4436c5b7921c3ae6b0ba514736f9cb0d
Kelly Dwan <ryelle@users.noreply.github.com>
2022-06-29 11:36:12 -0400

    Minify code only when running build

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,23 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+const plugins = [
+	// This has to run before any other plugins, to concatenate all files into one.
+	require( 'postcss-import' ),
+
+	// Enable transforms for stage 2+, explictly enable nesting (stage 1).
+	require( 'postcss-preset-env' )( {
+		stage: 2,
+		features: {
+			'nesting-rules': true,
+		},
+	} ),
+];
+
+// Minify.
+if ( isProduction ) {
+	plugins.push( require( 'cssnano' ) );
+}
+
 module.exports = {
-	plugins: [
-		// This has to run before any other plugins, to concatenate all files into one.
-		require( 'postcss-import' ),
-
-		// Enable transforms for all experimental features.
-		require( 'postcss-preset-env' )( {
-			stage: 0,
-		} ),
-
-		// Minify.
-		require( 'cssnano' ),
-	],
+	plugins,
 };
