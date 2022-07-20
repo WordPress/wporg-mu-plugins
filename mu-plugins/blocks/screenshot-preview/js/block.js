33bf0640827ea2285ca7fe41b6f93a5efeaffc9d
Steven Dufresne <steve.dufresne@automattic.com>
2022-07-21 08:57:44 +0900

    Add ScreenShot Preview Block (#220)

---
+++ mu-plugins/blocks/screenshot-preview/js/block.js
@@ -0,0 +1,56 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useRef, useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import getCardFrameHeight from './get-card-frame-height';
+import useInView from './in-view';
+import ScreenShot from './screenshot';
+
+function Block( { link, previewLink, version } ) {
+	const wrapperRef = useRef();
+	const [ frameHeight, setFrameHeight ] = useState( '1px' );
+	const isVisible = useInView( { element: wrapperRef } );
+	const [ shouldLoad, setShouldLoad ] = useState( false );
+
+	useEffect( () => {
+		const handleOnResize = () => {
+			try {
+				setFrameHeight( getCardFrameHeight( wrapperRef.current.clientWidth ) );
+			} catch ( err ) {}
+		};
+
+		handleOnResize();
+
+		window.addEventListener( 'resize', handleOnResize );
+
+		return () => {
+			window.addEventListener( 'resize', handleOnResize );
+		};
+	}, [ isVisible ] );
+
+	useEffect( () => {
+		if ( isVisible ) {
+			setShouldLoad( true );
+		}
+	}, [ isVisible ] );
+
+	return (
+		<a
+			className="wporg-screenshot-card"
+			ref={ wrapperRef }
+			style={ {
+				height: frameHeight,
+			} }
+			tabIndex="-1"
+			href={ link }
+		>
+			<ScreenShot src={ `${ previewLink }&version=${ version }` } isReady={ shouldLoad } />
+		</a>
+	);
+}
+
+export default Block;
