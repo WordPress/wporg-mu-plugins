c176f4ba546a00ed48a483bb60426c9f44f88ebe
dufresnesteven <steve.dufresne@automattic.com>
2022-07-22 14:20:29 +0900

    Load screenshot preview block.

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -7,6 +7,7 @@
  */
 
 require_once __DIR__ . '/blocks/global-header-footer/blocks.php';
+require_once __DIR__ . '/blocks/screenshot-preview/block.php';
 require_once __DIR__ . '/global-fonts/index.php';
 require_once __DIR__ . '/plugin-tweaks/index.php';
 require_once __DIR__ . '/rest-api/index.php';
