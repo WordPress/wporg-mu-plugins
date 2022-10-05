0b23fa84185a2d746ced2cef891a5b74aace4d3b
dufresnesteven <steve.dufresne@automattic.com>
2022-10-05 14:00:04 +0900

    Update js doc block.

--- mu-plugins/blocks/screenshot-preview/src/screenshot.js
+++ mu-plugins/blocks/screenshot-preview/src/screenshot.js
@@ -42,7 +42,7 @@ function useInterval( callback, delay ) {
  * we need to do some custom handling.
  *
  * @param {Object}  props
- * @param {string}  props.queryString Properties that are passed to mShots.
+ * @param {string}  props.queryString Arguments that are passed to mShots.
  * @param {string}  props.src         The url of the screenshot.
  * @param {boolean} props.isReady     Whether we should start try to show the image.
  *
