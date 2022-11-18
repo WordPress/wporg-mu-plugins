fc1f2af2aaf2607302e564f05e138b32d399f541
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-18 16:39:56 -0500

    Add a default subset to prevent warnings

--- mu-plugins/global-fonts/helper-functions.php
+++ mu-plugins/global-fonts/helper-functions.php
@@ -7,6 +7,6 @@
  * @param string $subsets The subset(s) to preload.
  * @return bool If the font will be preloaded.
  */
-function global_fonts_preload( $fonts, $subsets ) {
+function global_fonts_preload( $fonts, $subsets = '' ) {
 	return WordPressdotorg\MU_Plugins\Global_Fonts\preload_font( $fonts, $subsets );
 }
