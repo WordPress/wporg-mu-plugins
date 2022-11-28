594a05d209e7a0245a6aafce62fd8728a0536358
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-28 18:04:59 -0500

    Blocks: Add the "Site Breadcrumbs" block from showcase

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/blocks/horizontal-slider/horizontal-slider.php';
 require_once __DIR__ . '/blocks/language-suggest/language-suggest.php';
 require_once __DIR__ . '/blocks/latest-news/latest-news.php';
 require_once __DIR__ . '/blocks/screenshot-preview/block.php';
+require_once __DIR__ . '/blocks/site-breadcrumbs/index.php';
 require_once __DIR__ . '/global-fonts/index.php';
 require_once __DIR__ . '/plugin-tweaks/index.php';
 require_once __DIR__ . '/rest-api/index.php';
