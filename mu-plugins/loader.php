012b257070dbf3245cc1bb17ddd8629821ee0054
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-17 17:36:11 -0500

    Add a Notice Block

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/blocks/global-header-footer/blocks.php';
 require_once __DIR__ . '/blocks/horizontal-slider/horizontal-slider.php';
 require_once __DIR__ . '/blocks/language-suggest/language-suggest.php';
 require_once __DIR__ . '/blocks/latest-news/latest-news.php';
+require_once __DIR__ . '/blocks/notice/index.php';
 require_once __DIR__ . '/blocks/screenshot-preview/block.php';
 require_once __DIR__ . '/blocks/site-breadcrumbs/index.php';
 require_once __DIR__ . '/global-fonts/index.php';
