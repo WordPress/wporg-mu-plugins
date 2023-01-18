07b8945d696802099085e31ff961dad1e330ea4a
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-17 22:12:45 -0500

    Update mu-plugins/blocks/notice/src/icon/index.js
    Co-authored-by: Steven Dufresne <steve.dufresne@automattic.com>

--- mu-plugins/blocks/notice/src/icon/index.js
+++ mu-plugins/blocks/notice/src/icon/index.js
@@ -6,7 +6,7 @@ import { ReactComponent as Info } from './library/info.svg';
 import { ReactComponent as Tip } from './library/tip.svg';
 import { ReactComponent as Warning } from './library/warning.svg';
 
-export default function ( { type /*, label */ } ) {
+export default function ( { type } ) {
 	switch ( type ) {
 		case 'alert':
 			return <Alert />;
