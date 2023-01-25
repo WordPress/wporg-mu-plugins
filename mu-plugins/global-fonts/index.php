ab1a05ffeeac8fe0560e0add65b0a579b2566fc0
Steven Dufresne <steve.dufresne@automattic.com>
2023-01-26 08:17:59 +0900

    Add ibm mono medium. (#327)

--- mu-plugins/global-fonts/index.php
+++ mu-plugins/global-fonts/index.php
@@ -169,6 +169,10 @@ function get_font_url( $font, $subset ) {
 			$font_folder = 'IBMPlexMono/';
 			$font_file_name = 'IBMPlexMono-Italic-';
 			break;
+		case 'ibm plex mono medium':
+			$font_folder = 'IBMPlexMono/';
+			$font_file_name = 'IBMPlexMono-Medium-';
+			break;
 		case 'ibm plex mono bold':
 			$font_folder = 'IBMPlexMono/';
 			$font_file_name = 'IBMPlexMono-Bold-';
