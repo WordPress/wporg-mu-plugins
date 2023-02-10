29bf3db6dc70c375856bf22ecf7a1c0500b1a494
Kelly Dwan <ryelle@users.noreply.github.com>
2023-02-10 15:34:35 -0500

    Try: Override callout shortcodes in favor of the notice block

--- mu-plugins/blocks/notice/index.php
+++ mu-plugins/blocks/notice/index.php
@@ -6,7 +6,8 @@
 
 namespace WordPressdotorg\MU_Plugins\Notice;
 
-add_action( 'init', __NAMESPACE__ . '\init' );
+// Run after `WPorg_Handbook_Callout_Boxes` registers the shortcodes.
+add_action( 'init', __NAMESPACE__ . '\init', 11 );
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
@@ -17,4 +18,47 @@ add_action( 'init', __NAMESPACE__ . '\init' );
  */
 function init() {
 	register_block_type( __DIR__ . '/build' );
+
+	foreach ( [ 'info', 'tip', 'alert', 'tutorial', 'warning' ] as $shortcode ) {
+		remove_shortcode( $shortcode );
+		add_shortcode( $shortcode, __NAMESPACE__ . '\render_shortcode' );
+	}
+}
+
+/**
+ * Display the callout shortcodes using the notice block.
+ *
+ * @param array|string $attr    Shortcode attributes array or empty string.
+ * @param string       $content Shortcode content.
+ * @param string       $tag     Shortcode name.
+ *
+ * @return string Shortcode output as HTML markup.
+ */
+function render_shortcode( $attr, $content = '', $tag ) {
+	$shortcode_mapping = array(
+		'info'     => 'info',
+		'tip'      => 'tip',
+		'alert'    => 'alert',
+		'tutorial' => 'tip',
+		'warning'  => 'warning',
+	);
+
+	$type = $shortcode_mapping[ $tag ] ?: 'tip';
+
+	// Sanitize message content.
+	$content = wp_kses_post( $content );
+	// Temporarily disable o2 processing while formatting content.
+	add_filter( 'o2_process_the_content', '__return_false', 1 );
+	$content = apply_filters( 'the_content', $content );
+	remove_filter( 'o2_process_the_content', '__return_false', 1 );
+
+	$block_markup = <<<EOT
+<!-- wp:wporg/notice {"type":"$type"} -->
+<div class="wp-block-wporg-notice is-{$type}-notice">
+<div class="wp-block-wporg-notice__icon"></div>
+<div class="wp-block-wporg-notice__content">$content</div></div>
+<!-- /wp:wporg/notice -->
+EOT;
+
+	return do_blocks( $block_markup );
 }
