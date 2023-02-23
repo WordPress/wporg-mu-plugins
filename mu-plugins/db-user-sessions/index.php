0d2f3d29e0e37f1202a63c7493da52c571985dfa
Dion Hulse <dion@wordpress.org>
2023-02-23 08:18:26 +0000

    Only load the custom session handler on WordPress.org

--- mu-plugins/db-user-sessions/index.php
+++ mu-plugins/db-user-sessions/index.php
@@ -1,8 +1,12 @@
 <?php
 namespace WordPressdotorg\MU_Plugins\DB_User_Sessions;
 
-add_filter( 'session_token_manager', function() {
-	require_once __DIR__ . '/class-tokens.php';
+add_filter( 'session_token_manager', function( $manager ) {
+	if ( in_array( wp_get_environment_type(), [ 'production', 'staging' ], true ) ) {
+		require_once __DIR__ . '/class-tokens.php';
 
-	return __NAMESPACE__ . '\Tokens';
+		$manager = __NAMESPACE__ . '\Tokens';
+	}
+
+	return $manager;
 } );
