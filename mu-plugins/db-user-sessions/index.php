ae2528ed6192252af019cc3a9b376818eee58af9
Dion Hulse <dion@wordpress.org>
2023-02-24 14:22:06 +1000

    Document the Database schema in the file. Just for reference.

--- mu-plugins/db-user-sessions/index.php
+++ mu-plugins/db-user-sessions/index.php
@@ -11,3 +11,20 @@ add_filter( 'session_token_manager', function( $manager ) {
 
 	return $manager;
 } );
+
+/*
+Database schema:
+CREATE TABLE `wporg_user_sessions` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `verifier` char(64) NOT NULL,
+  `expiration` int(10) unsigned NOT NULL,
+  `ip` varbinary(16) DEFAULT NULL,
+  `login` int(10) unsigned NOT NULL,
+  `session_meta` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id__verifier` (`user_id`,`verifier`),
+  KEY `ip` (`ip`),
+  KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+*/
