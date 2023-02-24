7858204bc78c4e72b239f5b7aca1cb322027f404
Dion Hulse <dion@wordpress.org>
2023-02-24 14:09:32 +1000

    Use the autoloader for including the session manager.

--- mu-plugins/db-user-sessions/index.php
+++ mu-plugins/db-user-sessions/index.php
@@ -3,8 +3,6 @@ namespace WordPressdotorg\MU_Plugins\DB_User_Sessions;
 
 add_filter( 'session_token_manager', function( $manager ) {
 	if ( in_array( wp_get_environment_type(), [ 'production', 'staging' ], true ) ) {
-		require_once __DIR__ . '/class-tokens.php';
-
 		$manager = __NAMESPACE__ . '\Tokens';
 
 		// The user sesions are global, not per-site.
