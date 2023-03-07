e2a2264f68ed312eb4794d972828b160ec351c83
dufresnesteven <steve.dufresne@automattic.com>
2023-03-07 15:49:39 +0900

    Add no-js then remove it.

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -1,5 +1,6 @@
 <?php
 namespace WordPressdotorg\MU_Plugins;
+
 use WordPressdotorg\Autoload;
 
 /**
@@ -25,6 +26,7 @@ require_once __DIR__ . '/blocks/screenshot-preview/block.php';
 require_once __DIR__ . '/blocks/site-breadcrumbs/index.php';
 require_once __DIR__ . '/blocks/table-of-contents/index.php';
 require_once __DIR__ . '/global-fonts/index.php';
+require_once __DIR__ . '/no-js/index.php';
 require_once __DIR__ . '/plugin-tweaks/index.php';
 require_once __DIR__ . '/rest-api/index.php';
 require_once __DIR__ . '/skip-to/skip-to.php';
---
+++ mu-plugins/no-js/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: No-JS
+ * Description: This adds a 'no-js' class and removes it with JS for styling.
+ */
+
+namespace WordPressdotorg\MU_Plugins\NO_JS;
+
+// Actions & Filters
+add_filter( 'language_attributes', __NAMESPACE__ . '\add_no_js_class_to_html_tag', 10, 2 );
+add_action( 'wp_head', __NAMESPACE__ . '\add_custom_js' );
+
+/**
+ * Add a 'no-js' tag by default.
+ *
+ * @param string $output
+ * @param string $doctype
+ * @return string
+ */
+function add_no_js_tag( $output, $doctype ) {
+	if ( 'html' !== $doctype ) {
+		return $output;
+	}
+
+	$output .= ' class="no-js"';
+
+	return $output;
+}
+
+/**
+ * Remove the 'no-js' class from html using JS.
+ */
+function remove_js_tag() {
+	?>
+	<script>
+		document.documentElement.classList.remove('no-js')
+	</script>
+	<?php
+}
