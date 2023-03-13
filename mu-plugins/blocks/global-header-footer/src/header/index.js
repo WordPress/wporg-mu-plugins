54a68b130c6ba6d55d447514623b2bc005699fc7
Kelly Dwan <ryelle@users.noreply.github.com>
2023-03-13 17:33:46 -0400

    Remove the unnecessary variations code

--- mu-plugins/blocks/global-header-footer/src/header/index.js
+++ mu-plugins/blocks/global-header-footer/src/header/index.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
 import { Disabled } from '@wordpress/components';
 import { registerBlockType } from '@wordpress/blocks';
 import ServerSideRender from '@wordpress/server-side-render';
@@ -11,12 +10,6 @@ import { useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import metadata from './block.json';
-
-const variations = [
-	{ label: __( 'White on black (default)', 'wporg' ), value: 'white-on-black' },
-	{ label: __( 'Black on white', 'wporg' ), value: 'black-on-white' },
-	{ label: __( 'White on blue', 'wporg' ), value: 'white-on-blue' },
-];
 
 const Edit = ( { attributes } ) => (
 	<div { ...useBlockProps() }>
@@ -28,13 +21,4 @@ const Edit = ( { attributes } ) => (
 
 registerBlockType( metadata.name, {
 	edit: Edit,
-
-	variations: variations.map( ( { value, label } ) => ( {
-		name: value,
-		/* translators: %s is the color scheme label. */
-		title: sprintf( __( 'Global Header: %s', 'wporg' ), label ),
-		isActive: ( blockAttributes, variationAttributes ) => blockAttributes.style === variationAttributes.style,
-		scope: [ 'transform' ],
-		attributes: { style: value },
-	} ) ),
 } );
