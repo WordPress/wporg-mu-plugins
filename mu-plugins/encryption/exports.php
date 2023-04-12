482235dbe05f734bc2e8d50a9da694eeec7dbe57
Dion Hulse <dion@wordpress.org>
2023-04-12 07:24:53 +0000

    Add initial Encryption methods.

---
+++ mu-plugins/encryption/exports.php
@@ -0,0 +1,56 @@
+<?php
+use WordPressdotorg\MU_Plugins\Encryption\HiddenString;
+/**
+ * This file contains globally-exported function names for the Encryption plugin.
+ */
+
+/**
+ * Encrypt a value.
+ *
+ * Unlike the Encryption plugin, this function simply returns false for any errors, and
+ * HiddenStrings that can be cast to string as needed.
+ *
+ * @param string $value The plaintext value.
+ * @param string $key   The key to use for encryption. Optional.
+ * @return string|false The encrypted value, or false on error.
+ */
+function wporg_encrypt( string $value, string $key = '' ) {
+	try {
+		$value = \WordPressdotorg\MU_Plugins\Encryption\encrypt( $value, '', $key );
+
+		return new HiddenString( $value->getString(), false );
+	} catch ( Exception $e ) {
+		return false;
+	}
+}
+
+/**
+ * Decrypt a value.
+ *
+ * Unlike the Encryption plugin, this function simply returns false for any errors, and
+ * HiddenStrings that can be cast to string as needed.
+ *
+ * @param string $value The encrypted value.
+ * @param string $key   The key to use for decryption. Optional.
+ * @return string|false The decrypted value, or false on error.
+ */
+function wporg_decrypt( string $value, string $key = '' ) {
+	try {
+		$value = \WordPressdotorg\MU_Plugins\Encryption\decrypt( $value, '', $key );
+
+		return new HiddenString( $value->getString(), false );
+	} catch ( Exception $e ) {
+		return false;
+	}
+}
+
+/**
+ * Determine if a value is encrypted.
+ *
+ * @param string $value The value to check.
+ * @param string $key   Check if it uses this key. Optional. If not specified, it only validates that it's encrypted.
+ * @return bool True if the value is encrypted, false otherwise.
+ */
+function wporg_is_encrypted( string $value, string $key = '' ) : bool {
+	return \WordPressdotorg\MU_Plugins\Encryption\is_encrypted( $value, $key );
+}
---
+++ mu-plugins/encryption/index.php
@@ -0,0 +1,160 @@
+<?php
+namespace WordPressdotorg\MU_Plugins\Encryption;
+use Exception;
+/**
+ * Plugin Name: WordPress.org Encryption
+ * Description: Encryption functions for use on WordPress.org.
+ */
+require __DIR__ . '/exports.php';
+
+/**
+ * Prefix for encrypted secrets. Contains a version identifier.
+ *
+ * $t1$ -> v1 (RFC 6238, encrypted with XChaCha20-Poly1305, with a key derived from HMAC-SHA256
+ *      of the defined key.
+ *
+ * @var string
+ */
+const PREFIX = '$t1$';
+
+/**
+ * The length of the keys.
+ *
+ * @var int
+ */
+const KEY_LENGTH   = SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_KEYBYTES;
+const NONCE_LENGTH = SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_NPUBBYTES;
+
+/**
+ * Encrypt a value.
+ *
+ * @param string $value           Value to encrypt.
+ * @param string $additional_data Additional data to include in the encryption. Optional.
+ * @param string $key             Key to use for encryption. Optional.
+ * @return string Encrypted value, exceptions thrown on error.
+ */
+function encrypt( $value, string $additional_data = '', string $key = '' ) {
+	$key       = get_encryption_key( $key );
+	$nonce     = random_bytes( NONCE_LENGTH );
+	if ( ! $key || ! $nonce ) {
+		throw new Exception( 'Unable to create a nonce.' );
+	}
+
+	if ( $value instanceOf HiddenString ) {
+		$value = $value->getString();
+	}
+
+	$encrypted = sodium_crypto_aead_xchacha20poly1305_ietf_encrypt( $value, $additional_data, $nonce, $key->getString() );
+
+	sodium_memzero( $value );
+
+	return new HiddenString( PREFIX . sodium_bin2hex( $nonce . $encrypted ) );
+}
+
+/**
+ * Decrypt a value.
+ *
+ * @param string $value           Value to decrypt.
+ * @param string $additional_data Additional data to include in the encryption. Optional.
+ * @param string $key             Key to use for decryption. Optional.
+ * @return string Decrypted value.
+ */
+function decrypt( $value, string $additional_data = '', string $key = '' ) : HiddenString {
+	// Fetch the encryption key.
+	$key = get_encryption_key( $key );
+	if ( ! $key ) {
+		throw new Exception( 'Unable to get the encryption key.' );
+	}
+
+	if ( $value instanceOf HiddenString ) {
+		$value = $value->getString();
+	}
+
+	if ( ! is_encrypted( $value ) ) {
+		throw new Exception( 'Value is not encrypted.' );
+	}
+
+	// Remove the prefix, and convert back to binary.
+	$value = mb_substr( $value, mb_strlen( PREFIX, '8bit' ), null, '8bit' );
+	$value = sodium_hex2bin( $value );
+
+	if ( mb_strlen( $value, '8bit' ) < NONCE_LENGTH ) {
+		throw new Exception( 'Invalid cipher text' );
+	}
+
+	$nonce     = mb_substr( $value, 0, NONCE_LENGTH, '8bit' );
+	$value     = mb_substr( $value, NONCE_LENGTH, null, '8bit' );
+	$plaintext = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt( $value, $additional_data, $nonce, $key->getString() );
+
+	sodium_memzero( $nonce );
+	sodium_memzero( $value );
+
+	if ( false === $plaintext ) {
+		throw new Exception( 'Invalid cipher text' );
+	}
+
+	return new HiddenString( $plaintext );
+}
+
+/**
+ * Check if a value is encrypted.
+ *
+ * @param string $value Value to check.
+ * @param string $key   Key to use for decryption. Optional.
+ * @return bool True if the value is encrypted, false otherwise.
+ */
+function is_encrypted( $value, string $key = '' ) {
+	if ( $value instanceOf HiddenString ) {
+		$value = $value->getString();
+	}
+
+	if ( ! str_starts_with( $value, PREFIX ) ) {
+		return false;
+	}
+
+	if ( mb_strlen( $value, '8bit' ) < NONCE_LENGTH + strlen( PREFIX ) ) {
+		return false;
+	}
+
+	// Check if that's the key.
+	if ( $key ) {
+		try {
+			if ( ! decrypt( $value, $key ) ) {
+				return false;
+			}
+		} catch ( Exception $e ) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Get the encryption key.
+ *
+ * @param string $key The key to use for decryption.
+ * @return string The encryption key.
+ */
+function get_encryption_key( $key = '' ) {
+	$constant = 'WPORG_ENCRYPTION_KEY';
+
+	if ( $key ) {
+		$constant = 'WPORG_' . str_replace( '-', '_', strtoupper( $key ) ) . '_ENCRYPTION_KEY';
+	}
+
+	if ( ! defined( $constant ) ) {
+		throw new Exception( sprintf( 'Encryption key "%s" not defined.', $constant ) );
+	}
+
+	return new HiddenString( sodium_hex2bin( constant( $constant ) ) );
+}
+
+/**
+ * Generate a random encryption key.
+ *
+ * @return string The encryption key.
+ */
+function generate_encryption_key() {
+	return sodium_bin2hex( random_bytes( KEY_LENGTH ) );
+}
