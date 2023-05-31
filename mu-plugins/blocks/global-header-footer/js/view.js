WordPress/wporg-mu-plugins

commit 43a8a94a4de8cd16ac2b079c9eeb70eb71146a1d

Header: Set logo to `fit-content` to prevent pushing the locale off screen
Fixes #399

mu-plugins/blocks/global-header-footer/postcss/header/logos.pcss
@@ -9,7 +9,7 @@
 	}
 
 	& .wp-block-image.global-header__wporg-logo-mark {
-		flex-basis: 100%;
+		flex-basis: fit-content;
 		flex-shrink: 1;
 		text-align: left;
 

commit 514ec641aec9f89096da8be5892cf7b60ebcc856

Header: Fix header & nav issues on small screens (#401)
* Header: Fix logo spacing on default English site

Fixes issue introduced in https://github.com/WordPress/wporg-mu-plugins/commit/43a8a94a4de8cd16ac2b079c9eeb70eb71146a1d where the menu & search icons collapsed to the left on small screens. The logo flex size needs to be different when there is no locale name.

* Header: Only apply page scroll fix to header modals

This prevents other nav modals (like the page subnav) which toggle the has-modal-open class from affecting the position of the global header bar.

See #392, Fixes https://github.com/WordPress/wporg-parent-2021/issues/85

mu-plugins/blocks/global-header-footer/js/view.js
@@ -185,6 +185,22 @@
 			closeSearchButton.setAttribute( 'aria-label', labels.closeSearchLabel || 'Close Search' );
 		}
 
+		// Watch for the `has-modal-open` class to be removed, and remove the global class too.
+		// This works as a callback to be fired when the global header modals are closed, as
+		// they're attached when each modal opens.
+		const modalCloseObserver = new window.MutationObserver( ( mutationList, observer ) => {
+			for ( const mutation of mutationList ) {
+				const { attributeName, type, target } = mutation;
+				if ( type === 'attributes' && attributeName === 'class' ) {
+					if ( ! target.classList.contains( 'has-modal-open' ) ) {
+						target.classList.remove( 'has-global-modal-open' );
+					}
+				}
+			}
+			// Remove the observer to prevent recursion. This will be re-attached when the modal is opened.
+			observer.disconnect();
+		} );
+
 		const openButtons = document.querySelectorAll( '[data-micromodal-trigger]' );
 		openButtons.forEach( function ( button ) {
 			// When any open menu button is clicked, find any existing close buttons and click them.
@@ -198,6 +214,15 @@
 				);
 
 				closeButtons.forEach( ( _button ) => _button.click() );
+
+				// If this button opened the global search, add a class and trigger the close observer.
+				if (
+					button.parentNode.classList.contains( 'global-header__navigation' ) ||
+					button.parentNode.classList.contains( 'global-header__search' )
+				) {
+					document.documentElement.classList.add( 'has-global-modal-open' );
+					modalCloseObserver.observe( document.documentElement, { attributes: true } );
+				}
 			} );
 		} );
 	} );

mu-plugins/blocks/global-header-footer/postcss/header/logos.pcss
@@ -95,3 +95,11 @@
 		}
 	}
 }
+
+/* When on the main (non-rosetta) sites, the logo should take over 100% space. */
+html[lang="en-US"] .wp-block-group.global-header .wp-block-image.global-header__wporg-logo-mark {
+
+	@media (--mobile-only) {
+		flex-basis: 100%;
+	}
+}

mu-plugins/blocks/global-header-footer/postcss/header/menu.pcss
@@ -334,7 +334,7 @@
  * See https://github.com/WordPress/wporg-mu-plugins/issues/371/.
  */
 @media (--mobile-only) {
-	html.has-modal-open .global-header {
+	html.has-global-modal-open .global-header {
 		position: fixed;
 		z-index: 9999; /* Arbitrary z-index to be above any possible element on a page. */
 		left: 0;