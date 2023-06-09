5b262dba5521a1431a14acd72883ebb5137f5744
StevenDufresne <dufresnesteven@gmail.com>
2023-06-09 11:02:05 +0900

    Rest API: Add an endpoint to store stats related data.
    Code Review Completed: https://github.com/WordPress/wordpress.org/pull/144

--- mu-plugins/rest-api/index.php
+++ mu-plugins/rest-api/index.php
@@ -16,6 +16,7 @@ add_filter( 'rest_user_query', __NAMESPACE__ . '\modify_user_query_parameters', 
  */
 function initialize_rest_endpoints() {
 	require_once __DIR__ . '/endpoints/class-wporg-rest-users-controller.php';
+	require_once __DIR__ . '/endpoints/class-wporg-site-quality-controller.php';
 
 	$users_controller = new Users_Controller();
 	$users_controller->register_routes();
