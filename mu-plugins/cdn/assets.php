d47d78125f7a7d4be44dfb3a264fe409f15cf5b4
Dion Hulse <dion@wordpress.org>
2023-08-22 01:55:15 +0000

    Switch to using a file hash for the buster, to avoid multi-server filemtime differences. Cache the file hashes using APCu. Move the mid-deploy cache buster into a separate function. Apply to URLs generated by `plugins_url()`.

--- mu-plugins/cdn/assets.php
+++ mu-plugins/cdn/assets.php
@@ -10,18 +10,17 @@ namespace WordPressdotorg\MU_Plugins\CDN;
  *  - Only applies to URLs which already include ?ver= cache-busters, and do NOT contain other parameters.
  *
  * In CDN'ing, there's a few specific changes made:
- *  - ver= is always changed to the filemtime of the asset (if not already), for consistent and easy cache-busting.
+ *  - ver= is always changed to a hash of the asset contents, for consistent and easy cache-busting.
  *  - assets are shared between sites, all use s.w.org/* instead of wordpress.org/wp-includes/* or wordpress.org/plugins/wp-includes/*.
- *  - non-production wp_get_envionment_type() skips CDN'isation.
+ *  - non-production wp_get_envionment_type() skips CDN'isation by default, but can be overridden with a constant.
  *  - During a deploy, any file that has recently been modified temporarily uses a different cache buster until the deploy is finished.
  *    This avoids scenario's where a web-node references a newer version of the file, but the subsequent request is served by a node
  *    which hasn't yet got that new version of the file, which would then otherwise be cached by the CDN with the new version identifier.
  *
  * @param string $link   The non-CDNised URL.
- * @param string $handle The asset handle, used to skip certain assets.
  * @return string The potentially CDNised URL.
  */
-function with_filemtime_cachebuster( $link, $handle = '' ) {
+function with_filemtime_cachebuster( $link ) {
 	$hostname = strtolower( wp_parse_url( $link, PHP_URL_HOST ) );
 
 	// Only WordPress.org domain files
@@ -34,7 +33,7 @@ function with_filemtime_cachebuster( $link, $handle = '' ) {
 		'profiles.wordpress.org',
 		'events.wordpress.org',
 	];
-	if ( in_array( $hostname, $other_networks, true ) {
+	if ( in_array( $hostname, $other_networks, true ) ) {
 		return $link;
 	}
 
@@ -67,39 +66,45 @@ function with_filemtime_cachebuster( $link, $handle = '' ) {
 		return $link;
 	}
 
-	// Set the version to the file modification time, for consistency.
-	$version = false;
-	if ( ! is_timestamp( $url_args['ver'] ) && file_exists( ABSPATH . $filepath ) ) {
-		$version = filemtime( ABSPATH . $filepath );
-	}
-	if ( ! $version ) {
-		$version = $url_args['ver'];
+	// Find the modified time for the file, which may already be the version.
+	$timestamp = $url_args['ver'];
+	if ( ! is_timestamp( $timestamp ) ) {
+		if ( file_exists( ABSPATH . $filepath ) ) {
+			$timestamp = filemtime( ABSPATH . $filepath );
+		} else {
+			$timestamp = null;
+		}
 	}
 
-	// While a deploy is occuring, use a mid-deploy version for recently modified files.
-	wp_cache_add_global_groups( 'wporg_deploy' );
-	$is_mid_deploy = wp_cache_get( 'is_mid_deploy', 'wporg_deploy', false, $found );
-	if (
-		$found &&
-		$is_mid_deploy &&
-		is_timestamp( $version ) &&
-		// .. and cache buster version is a recent timestamp, older assets can re-use existing caches
-		$version > ( time() - HOUR_IN_SECONDS )
-	) {
-		$version .= '-mid';
-	}
+	/*
+	 * The version is set to the timestamp by default.
+	 * CDN will override this with a content hash.
+	 */
+	$version = $timestamp ?: $url_args['ver'];
 
 	// CDN is used in production by default.
 	$use_cdn = ( 'production' === wp_get_environment_type() );
 
-	// Allow other environments to opt-in via constant.
+	/*
+	 * Allow other environments to opt-in via constant.
+	 * Note: This is not recommended, except for testing.
+	 */
 	if ( defined( 'USE_WPORG_CDN' ) && USE_WPORG_CDN ) {
 		$use_cdn = true;
 	}
 
-	// If we're using the CDN, change the hostname.
+	// If we're using the CDN, make the needed changes.
 	if ( $use_cdn ) {
 		$hostname = 's.w.org';
+
+		/*
+		 * Use a cache-buster that's unique to this file contents.
+		 * This allows a cache to be consistent between servers with different modification times.
+		 */
+		$version = get_file_hash( $filepath, $timestamp ) ?: $timestamp;
+
+		// While a deploy is occuring, use a mid-deploy version recently modified files.
+		$version .= mid_deploy_cachebuster( $filepath, $timestamp );
 	}
 
 	// Generate the new link.
@@ -108,8 +113,9 @@ function with_filemtime_cachebuster( $link, $handle = '' ) {
 
 	return $link;
 }
-add_filter( 'style_loader_src', __NAMESPACE__ . '\with_filemtime_cachebuster', 5, 2 );
-add_filter( 'script_loader_src', __NAMESPACE__ . '\with_filemtime_cachebuster', 5, 2 );
+add_filter( 'style_loader_src',  __NAMESPACE__ . '\with_filemtime_cachebuster', 5 );
+add_filter( 'script_loader_src', __NAMESPACE__ . '\with_filemtime_cachebuster', 5 );
+add_filter( 'plugins_url',       __NAMESPACE__ . '\with_filemtime_cachebuster', 5 );
 
 /**
  * Determine if a string appears to be a timestamp.
@@ -126,4 +132,57 @@ function is_timestamp( $string ) {
 		$string >= 1262304000 /* Y2.01K - 2010-01-01 */ &&
 		$string <= time()
 	);
+}
+
+/**
+ * Fetch the file content cache-buster for a given file.
+ *
+ * @param string $file The file to fetch the cache-buster for.
+ * @param int    $time Optional. The file modification time, if already known.
+ * @return string The cache-buster. First 12 characters of the sha1.
+ */
+function get_file_hash( $file, $time = null ) {
+	static $cached = [];
+
+	$file   = trailingslashit( ABSPATH ) . $file;
+	$time ??= filemtime( $file );
+	$key    = $file . ':' . $time . ':hash';
+
+	if ( ! isset( $cached[ $key ] ) ) {
+		$cached[ $key ] = apcu_fetch( $key, $found );
+
+		if ( ! $found ) {
+			$cached[ $key ] = substr( sha1_file( $file ), 0, 12 );
+			apcu_store( $key, $cached[ $key ], DAY_IN_SECONDS );
+		}
+	}
+
+	return $cached[ $key ];
+}
+
+/**
+ * Return a mid-deploy cache buster, during deploys.
+ *
+ * @param string $file          The file to fetch the cache-buster for.
+ * @param int    $modified_time Optional. The file modification time, if already known.
+ * @return string A cache buster, if required, an empty string otherwise.
+ */
+function mid_deploy_cachebuster( $file, $modified_time = false ) {
+	wp_cache_add_global_groups( 'wporg_deploy' );
+
+	$is_mid_deploy = wp_cache_get( 'is_mid_deploy', 'wporg_deploy', false, $found );
+
+	if (
+		$found &&
+		$is_mid_deploy &&
+		(
+			! $modified_time ||
+			// .. and cache buster version is a recent timestamp, older assets can re-use existing caches
+			$modified_time > ( time() - HOUR_IN_SECONDS )
+		)
+	) {
+		return '-mid-deploy';
+	}
+
+	return '';
 }
