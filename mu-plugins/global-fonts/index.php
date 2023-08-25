cb5a05256f5f49d8facc9a0b54e48c3b0520bb04
Dion Hulse <dion@wordpress.org>
2023-08-25 12:25:49 +1000

    Revert CDN asset changes (#429)
    * Revert "Global Fonts: Fix the inline URLs after #424."
    * Revert "Add CDN versioning for fonts (#424)"
    * Revert "Add a CDNise mu-plugin (#173)"

--- mu-plugins/global-fonts/index.php
+++ mu-plugins/global-fonts/index.php
@@ -41,20 +41,9 @@ function relative_to_absolute_urls( $editor_settings ) {
 	}
 
 	foreach ( $editor_settings['styles'] as $i => $style ) {
-		if (
-			str_contains( $style['css'], './Inter' ) ||
-			str_contains( $style['css'], './EB-Garamond' ) ||
-			str_contains( $style['css'], './CourierPrime' ) ||
-			str_contains( $style['css'], './IBMPlexMono' )
-		) {
-			$style['css'] = preg_replace_callback(
-				'!url\(./(?P<path>[^)]+)\)!i',
-				function( $m ) {
-					return "url(" . plugins_url( $m['path'], __FILE__ ) . ")";
-				},
-				$style['css']
-			);
-
+		if ( str_contains( $style['css'], './Inter' ) || str_contains( $style['css'], './EB-Garamond' ) ) {
+			$url = plugins_url( '', __FILE__ );
+			$style['css'] = str_replace( 'url(./', "url($url/", $style['css'] );
 			$editor_settings['styles'][ $i ] = $style;
 		}
 	}
@@ -210,7 +199,5 @@ function get_font_url( $font, $subset ) {
 		return false;
 	}
 
-	$font_version = filemtime( __DIR__ . '/' . $filepath );
-
-	return plugins_url( "{$filepath}?ver={$font_version}", __FILE__ );
+	return plugins_url( $filepath, __FILE__ );
 }
