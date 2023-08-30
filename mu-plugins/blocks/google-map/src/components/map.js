cf21c8633a6677cf0f5b0e558a06a52680f19037
Ian Dunn <ian@iandunn.name>
2023-08-30 15:18:56 -0700

    Google Map: Add search feature

--- mu-plugins/blocks/google-map/src/components/map.js
+++ mu-plugins/blocks/google-map/src/components/map.js
@@ -6,14 +6,19 @@ import GoogleMapReact from 'google-map-react';
 /**
  * WordPress dependencies
  */
-import { useCallback, useState } from '@wordpress/element';
+import { useCallback, useEffect, useState } from '@wordpress/element';
 import { Spinner } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { mapStyles } from '../utilities/map-styles';
-import { createClusteredMarkers } from '../utilities/google-maps-api';
+import {
+	assignMarkerReferences,
+	clusterMarkers,
+	panToCenter,
+	setVisibleMarkers,
+} from '../utilities/google-maps-api';
 
 /**
  * Render a Google Map with info windows for the given markers.
@@ -29,6 +34,9 @@ import { createClusteredMarkers } from '../utilities/google-maps-api';
  */
 export default function Map( { apiKey, markers, icon } ) {
 	const [ loaded, setLoaded ] = useState( false );
+	const [ clusterer, setClusterer ] = useState( null );
+	const [ googleMap, setGoogleMap ] = useState( null );
+	const [ googleMapsApi, setGoogleMapsApi ] = useState( null );
 
 	const options = {
 		zoomControl: true,
@@ -43,9 +51,40 @@ export default function Map( { apiKey, markers, icon } ) {
 	 * Callback for `onGoogleApiLoaded`.
 	 */
 	const mapLoaded = useCallback( ( { map, maps } ) => {
-		createClusteredMarkers( map, maps, markers, icon );
+		if ( ! map || ! maps ) {
+			throw 'Google Maps API is not loaded.';
+		}
+
+		setGoogleMap( map );
+		setGoogleMapsApi( maps );
+
+		markers = assignMarkerReferences( map, maps, markers, icon );
+
+		setClusterer(
+			clusterMarkers(
+				map,
+				maps,
+				markers.map( ( marker ) => marker.markerRef ),
+				icon
+			)
+		);
+
 		setLoaded( true );
 	}, [] );
+
+	/**
+	 * Update the map whenever the supplied markers change.
+	 */
+	useEffect( () => {
+		if ( ! clusterer ) {
+			return;
+		}
+
+		const markerObjects = markers.map( ( marker ) => marker.markerRef );
+
+		setVisibleMarkers( clusterer, markerObjects, googleMap );
+		panToCenter( markerObjects, googleMap, googleMapsApi );
+	}, [ clusterer, markers ] );
 
 	return (
 		<div className="wporg-google-map__container">
