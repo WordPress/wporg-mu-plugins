7585c683b718cb2b2a1a07b4140f836889230b81
Kelly Dwan <ryelle@users.noreply.github.com>
2023-09-04 16:29:21 -0400

    Refactor filters block

---
+++ mu-plugins/blocks/query-filter/render.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Render the query filter.
+ */
+
+/**
+ * TBD
+ *
+ * @param string   $options Array of options in format [tbd].
+ * @param WP_Block $block   The current block being rendered.
+ */
+$filter = apply_filters( "wporg_query_filter_{$attributes['key']}", array(), $block );
+
+$init_state = [
+	'isOpen' => false,
+	'hasHover' => false,
+];
+$encoded_state = wp_json_encode( [ 'wporg' => [ 'queryFilter' => $init_state ] ] );
+
+// Use the current URL, including query parameters, to build up form action.
+$current_path = '';
+if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+	$current_path = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
+}
+$form_url = site_url( $current_path );
+
+$html_id = "filter-{$filter['key']}";
+?>
+<div
+	<?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>
+	data-wp-interactive
+	data-wp-context="<?php echo esc_attr( $encoded_state ); ?>"
+	data-wp-on--focusout="actions.wporg.queryFilter.handleFocusout"
+	data-wp-on--mouseenter="actions.wporg.queryFilter.handleMouseEnter"
+	data-wp-on--mouseleave="actions.wporg.queryFilter.handleMouseLeave"
+	data-wp-effect="effects.wporg.queryFilter.init"
+>
+	<button
+		class="wporg-query-filter__toggle"
+		data-wp-class--is-active="context.wporg.queryFilter.isOpen"
+		data-wp-on--click="actions.wporg.queryFilter.toggle"
+		data-wp-bind--aria-expanded="context.wporg.queryFilter.isOpen"
+		aria-controls="<?php echo esc_attr( $html_id ); ?>"
+	><?php echo wp_kses_post( $filter['label'] ); ?></button>
+
+	<div
+		class="wporg-query-filter__modal"
+		id="<?php echo esc_attr( $html_id ); ?>"
+		data-wp-bind--hidden="!context.wporg.queryFilter.isOpen"
+		data-wp-effect="effects.wporg.queryFilter.focusFirstElement"
+		data-wp-on--keydown="actions.wporg.queryFilter.handleKeydown"
+	>
+		<form action="<?php echo esc_attr( $form_url ); ?>">
+			<div class="wporg-query-filter__modal-content">
+				<?php foreach ( $filter['options'] as $value => $label ) : ?>
+				<div class="wporg-query-filter__option">
+					<input type="checkbox" name="<?php echo esc_attr( $filter['key'] ); ?>[]" value="<?php echo esc_attr( $value ); ?>" id="<?php echo esc_attr( $html_id . '-' . $value ); ?>" />
+					<label for="<?php echo esc_attr( $html_id . '-' . $value ); ?>"><?php echo esc_html( $label ); ?></label>
+				</div>
+				<?php endforeach; ?>
+			</div>
+
+			<div class="wporg-query-filter__modal-actions">
+				<input type="reset" value="<?php esc_attr_e( 'Clear', 'wporg' ); ?>" />
+				<input type="submit" value="<?php esc_html_e( 'Apply', 'wporg' ); ?>" />
+			</div> <!-- /.wporg-query-filter__actions -->
+		</form>
+	</div> <!-- /.wporg-query-filter__modal -->
+</div> <!-- /.wporg-query-filter -->
---
+++ mu-plugins/blocks/query-filter/src/view.js
@@ -0,0 +1,90 @@
+/**
+ * WordPress dependencies
+ */
+import { store as wpStore } from '@wordpress/interactivity';
+
+// See https://github.com/WordPress/gutenberg/blob/37f52ae884a40f7cb77ac2484648b4e4ad973b59/packages/block-library/src/navigation/view-interactivity.js
+const focusableSelectors = [
+	'a[href]',
+	'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+	'select:not([disabled]):not([aria-hidden])',
+	'textarea:not([disabled]):not([aria-hidden])',
+	'button:not([disabled]):not([aria-hidden])',
+	'[contenteditable]',
+	'[tabindex]:not([tabindex^="-"])',
+];
+
+wpStore( {
+	actions: {
+		wporg: {
+			queryFilter: {
+				toggle: ( { context } ) => {
+					context.wporg.queryFilter.isOpen = ! context.wporg.queryFilter.isOpen;
+				},
+				handleFocusout: ( { context, event, ref } ) => {
+					if (
+						! context.wporg.queryFilter.hasHover &&
+						! ref.contains( event.relatedTarget ) &&
+						event.target !== window.document.activeElement
+					) {
+						context.wporg.queryFilter.isOpen = false;
+					}
+				},
+				handleKeydown: ( { context, event } ) => {
+					// If Escape close the dropdown.
+					if ( event.key === 'Escape' ) {
+						context.wporg.queryFilter.isOpen = false;
+						context.wporg.queryFilter.toggleButton.focus();
+						return;
+					}
+
+					// Trap focus.
+					if ( event.key === 'Tab' ) {
+						// If shift + tab it change the direction.
+						if (
+							event.shiftKey &&
+							window.document.activeElement === context.wporg.queryFilter.firstFocusableElement
+						) {
+							event.preventDefault();
+							context.wporg.queryFilter.lastFocusableElement.focus();
+						} else if (
+							! event.shiftKey &&
+							window.document.activeElement === context.wporg.queryFilter.lastFocusableElement
+						) {
+							event.preventDefault();
+							context.wporg.queryFilter.firstFocusableElement.focus();
+						}
+					}
+				},
+				handleMouseEnter: ( { context } ) => {
+					context.wporg.queryFilter.hasHover = true;
+				},
+				handleMouseLeave: ( { context } ) => {
+					context.wporg.queryFilter.hasHover = null;
+				},
+			},
+		},
+	},
+	effects: {
+		wporg: {
+			queryFilter: {
+				init: ( { context, ref } ) => {
+					if ( context.wporg.queryFilter.isOpen ) {
+						const focusableElements = ref.querySelectorAll( focusableSelectors );
+						context.wporg.queryFilter.toggleButton = ref.querySelector(
+							'.wporg-query-filter__toggle'
+						);
+						context.wporg.queryFilter.firstFocusableElement = focusableElements[ 0 ];
+						context.wporg.queryFilter.lastFocusableElement =
+							focusableElements[ focusableElements.length - 1 ];
+					}
+				},
+				focusFirstElement: ( { context, ref } ) => {
+					if ( context.wporg.queryFilter.isOpen ) {
+						ref.querySelector( 'form input:first-child' ).focus();
+					}
+				},
+			},
+		},
+	},
+} );
