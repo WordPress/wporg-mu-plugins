1dc0679721b91a8f12e4db7965ef46f282997161
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-06 10:24:13 +1300

    Remove unused var

--- mu-plugins/blocks/local-navigation-bar/render.php
+++ mu-plugins/blocks/local-navigation-bar/render.php
@@ -4,12 +4,9 @@
  */
 
 use function WordPressdotorg\MU_Plugins\Global_Header_Footer\get_home_url;
-
-$wrapper_attributes = get_block_wrapper_attributes();
 ?>
-<div
-	<?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>
->
+
+<div <?php echo get_block_wrapper_attributes(); // phpcs:ignore ?> >
 	<figure class="wp-block-image global-header__wporg-logo-mark">
 		<a href="<?php echo esc_url( get_home_url() ); ?>">
 			<?php require dirname( __DIR__ ) . '/global-header-footer/images/w-mark.svg'; ?>
