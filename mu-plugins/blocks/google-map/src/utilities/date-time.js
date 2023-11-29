40a8a439db155419e4184050490c99b24599e226
Ian Dunn <ian@iandunn.name>
2023-11-29 08:55:47 -0800

    Map: Document importance of day of week and timezone in date format

--- mu-plugins/blocks/google-map/src/utilities/date-time.js
+++ mu-plugins/blocks/google-map/src/utilities/date-time.js
@@ -1,6 +1,13 @@
 /**
  * Display a timestamp in the user's timezone and locale format.
  *
+ * Note: The start time and day of the week are important pieces of information to include, since that helps
+ * attendees know at a glance if it's something they can attend. Otherwise they have to click to open it. The
+ * timezone is also important to make it clear that we're showing the user's timezone, not the venue's.
+ *
+ * @see https://make.wordpress.org/community/2017/03/23/showing-upcoming-local-events-in-wp-admin/#comment-23297
+ * @see https://make.wordpress.org/community/2017/03/23/showing-upcoming-local-events-in-wp-admin/#comment-23307
+ *
  * @param {number} timestamp
  *
  * @return {string}
