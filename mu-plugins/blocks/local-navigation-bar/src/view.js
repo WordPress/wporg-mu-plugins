43dc4eca4a0036bdceb6341f2a71d2e4cc331bb9
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-19 16:59:54 -0500

    Local Navigation: Improve style and behavior across screen sizes (#573)
    * Prevent menu from wrapping to second line
    
    * Use the block-specific hook to save a conditional
    
    * Move the custom property section to the top of the file
    
    * Style the mobile modal for medium & small screens, update focus states and spacing to match designs
    
    * Ensure child navigation block uses the expected attributes
    
    * Add an extra navigation block which is always collapsed, hidden by default
    
    * Adjust spacing & breakpoints
    
    * Update alignment of spacer dot
    
    * Hide the page title on small screens to prevent overflow
    
    * Prevent very long titles from wrapping on medium-small screens
    
    * Show a mobile menu (chevron dropdown) when the local nav and/or page title would cause the text to wrap
    
    * Get the gap value dynamically
    
    On some sites (like Documentation), this is 24px, while it's 20px on Developer.
    
    * Ensure the focus style appears inside the overflow boundary
    
    * Fix alignment issues
    
    * Local navs with bottom borders should only show the border on scroll
    
    * Add class to fade in content on scroll
    
    * Force section title elements to not wrap
    
    * Use more broad title selector, check before using
    
    * Switch to parseInt for integer conversion
    
    * Fix scrolling when mobile menu is longer than screen height
    
    * Fix height & dropdown spacing on mid-sized screens

--- mu-plugins/blocks/local-navigation-bar/src/view.js
+++ mu-plugins/blocks/local-navigation-bar/src/view.js
@@ -36,6 +36,67 @@ function init() {
 
 		document.addEventListener( 'scroll', debounce( onScroll ), { passive: true } );
 		onScroll();
+
+		// Check the size of child elements to determine if the local navigation
+		// menu should be collapsed in mobile-view by default. If so, toggle a
+		// CSS class to show the nav block with {"overlayMenu":"always"}
+		// added by `add_extra_navigation`.
+		const onResize = () => {
+			// Bail early on small screens, the visible nav block is already mobile.
+			if ( window.innerWidth <= 600 ) {
+				container.classList.remove( 'wporg-show-collapsed-nav' );
+				return;
+			}
+
+			// Fetch the navWidth from a data value which is set on page load,
+			// so that the uncollapsed visible menu's width is used.
+			let navWidth = container.dataset.navWidth;
+			if ( ! navWidth ) {
+				const navElement = container.querySelector( 'nav:not(.wporg-is-collapsed-nav)' );
+				const navGap = parseInt( window.getComputedStyle( navElement ).gap, 10 ) || 20;
+				// Get the nav width based on items, so that it stays
+				// consistent even if the menu wraps to a new line.
+				const menuItems = navElement.querySelectorAll( '.wp-block-navigation__container > li' );
+				navWidth =
+					[ ...menuItems ].reduce(
+						( acc, current ) => ( acc += current.getBoundingClientRect().width ),
+						0
+					) +
+					navGap * ( menuItems.length - 1 ); // 20px gap between items.
+
+				// Save the value for future resize callbacks.
+				container.dataset.navWidth = Math.ceil( navWidth );
+			}
+
+			const {
+				paddingInlineStart = '0px',
+				paddingInlineEnd = '0px',
+				gap = '0px',
+			} = window.getComputedStyle( container );
+
+			const availableWidth =
+				window.innerWidth -
+				parseInt( paddingInlineStart, 10 ) -
+				parseInt( paddingInlineEnd, 10 ) -
+				parseInt( gap, 10 );
+
+			const titleElement = container.querySelector( '.wp-block-site-title, div.wp-block-group' );
+			if ( ! titleElement ) {
+				return;
+			}
+			const { width: titleWidth } = titleElement.getBoundingClientRect();
+
+			const usedWidth = Math.ceil( titleWidth ) + Math.ceil( navWidth );
+
+			if ( usedWidth > availableWidth ) {
+				container.classList.add( 'wporg-show-collapsed-nav' );
+			} else {
+				container.classList.remove( 'wporg-show-collapsed-nav' );
+			}
+		};
+
+		window.addEventListener( 'resize', debounce( onResize ), { passive: true } );
+		onResize();
 	}
 }
 window.addEventListener( 'load', init );
