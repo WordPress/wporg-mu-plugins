3b648e09633fd34ecd914fafef57204095ad28b9
Kelly Dwan <ryelle@users.noreply.github.com>
2024-04-23 15:13:25 -0400

    Screenshot preview: Remove block code from React screenshot code

--- mu-plugins/blocks/screenshot-preview/block.php
+++ mu-plugins/blocks/screenshot-preview/block.php
@@ -15,36 +15,11 @@ defined( 'WPINC' ) || die();
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets', 20 );
 
 /**
- * Renders the `wporg/screenshot-preview` block on the server.
- *
- * @param array    $attributes Block attributes.
- * @param string   $content    Block default content.
- * @param WP_Block $block      Block instance.
- *
- * @return string Returns the event year for the current post.
- */
-function render_block( $attributes, $content ) {
-	if( ! isset( $attributes['link'] ) || ! isset( $attributes['preview-link'] ) ) {
-		return '';
-	}
-
-	return sprintf(
-		'<div 
-			class="wporg-screenshot-preview-js"
-			data-link="%1$s" 
-			data-preview-link="%2$s" 
-		></div>',
-		esc_attr( $attributes['link'] ),		
-		esc_attr( $attributes['preview-link'] )
-	);
-}
-
-/**
  * Register scripts, styles, and block.
  */
 function register_assets() {
 	$deps_path = __DIR__ . '/build/index.asset.php';
-	
+
 	if ( ! file_exists( $deps_path ) ) {
 		return;
 	}
@@ -67,20 +42,6 @@ function register_assets() {
 			filemtime( __DIR__ . '/build/style.css' )
 		);
 
-		wp_style_add_data( 'wporg-screenshot-preview-style', 'rtl', 'replace' ); 
+		wp_style_add_data( 'wporg-screenshot-preview-style', 'rtl', 'replace' );
 	}
-
-	register_block();
-}
-
-/**
- * Registers the `wporg/screenshot-preview` block on the server.
- */
-function register_block() {
-	register_block_type(
-		__DIR__ . '/block.json',
-		array(
-			'render_callback' => __NAMESPACE__ . '\render_block',
-		)
-	);
 }
