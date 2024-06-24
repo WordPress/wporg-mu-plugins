8291de71e73cdd3690ce5766c75cfb0a9650b5a6
StevenDufresne <dufresnesteven@gmail.com>
2024-06-24 13:54:11 +0900

    Move and adjust ratings-bars and ratings-stars.

---
+++ mu-plugins/blocks/ratings-bars/render.php
@@ -0,0 +1,42 @@
+<?php
+
+use function WordPressdotorg\Theme\Theme_Directory_2024\get_support_url;
+
+$attributes = apply_filters( 'wporg_block_ratings_bars', $attributes );
+
+if ( ! $attributes['num_ratings'] ) {
+	return;
+}
+
+?>
+<ul <?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>>
+<?php
+foreach ( range( 5, 1 ) as $stars ) :
+	if ( ! isset( $attributes['ratings'][ $stars - 1 ] ) ) {
+		continue;
+	}
+	$count = $attributes['ratings'][ $stars - 1 ];
+	$rating_bar_width = 100 * $count / $attributes['num_ratings'];
+	?>
+	<li class="wporg-ratings-bars__bar">
+		<a href="<?php echo esc_url( get_support_url( $attributes['slug'] . '/reviews/?filter=' . $stars ) ); ?>">
+			<span class="screen-reader-text">
+			<?php
+				// translators: %1$d: count of reviews. %2$d: level of star rating (ex, 5-star).
+				echo esc_html( sprintf( _n( '%1$d %2$d-star review', '%1$d %2$d-star reviews', $count, 'wporg-themes' ), $count, $stars ) );
+			?>
+			</span>
+			<span aria-hidden="true" class="wporg-ratings-bars__bar-label">
+			<?php
+				// translators: %d: star review amount, 1-5; ex "5 stars".
+				echo esc_html( sprintf( _n( '%d star', '%d stars', $stars, 'wporg-themes' ), $stars ) );
+			?>
+			</span>
+			<span aria-hidden="true" class="wporg-ratings-bars__bar-background">
+				<span class="wporg-ratings-bars__bar-foreground" style="width: <?php echo intval( $rating_bar_width ); ?>%;"></span>
+			</span>
+			<span aria-hidden="true" class="wporg-ratings-bars__bar-count"><?php echo intval( $count ); ?></span>
+		</a>
+	</li>
+<?php endforeach; ?>
+</ul>
