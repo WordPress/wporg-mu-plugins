450e02d7e6e27ac9525235eaa14c74615b912ecf
Kelly Dwan <ryelle@users.noreply.github.com>
2024-09-10 18:16:31 -0400

    Add a shortcode for nocookie youtube embeds

--- mu-plugins/plugin-tweaks/index.php
+++ mu-plugins/plugin-tweaks/index.php
@@ -13,3 +13,4 @@ require_once __DIR__ . '/wporg-internal-notes.php';
 require_once __DIR__ . '/stream.php';
 require_once __DIR__ . '/incompatible-plugins.php';
 require_once __DIR__ . '/gutenberg.php';
+require_once __DIR__ . '/youtube-shortcode.php';
