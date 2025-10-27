f793e78ac994284466424c6ae89553bb152d18e6
Dion Hulse <dion@wordpress.org>
2025-10-27 15:28:11 +1000

    Handle potential null parent in admin bar logic causing PHP Deprecated warning.

--- mu-plugins/blocks/global-header-footer/admin-bar.php
+++ mu-plugins/blocks/global-header-footer/admin-bar.php
@@ -69,7 +69,7 @@ function filter_admin_bar_links( $wp_admin_bar ) {
 				$ab_item->parent = 'edit-actions';
 				$wp_admin_bar->remove_node( $ab_item->id );
 				$edit_items[] = $ab_item;
-			} else if ( preg_match( '/blog-\d+/', $ab_item->parent ) ) {
+			} else if ( preg_match( '/blog-\d+/', $ab_item->parent ?? '' ) ) {
 				$wp_admin_bar->remove_node( $ab_item->id );
 			}
 		}
